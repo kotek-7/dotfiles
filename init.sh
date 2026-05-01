618eee72a7685d35f3587ec0654ac474dff44ccf
kotek <scpktk@gmail.com>
2026-05-01 19:54:18 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 apt-get update
-apt-get install -y build-essential curl git pkg-config libssl-dev
+apt-get install -y build-essential curl git zsh pkg-config libssl-dev
 
 # install brew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
 
-# install brew packages
-brew install zsh mise chezmoi
+# install chezmoi
+sh -c "$(curl -fsLS https://get.chezmoi.io)"
+
+# install mise
+sh -c "$(curl -fsSL https://mise.run)"
 
 # initialize dotfiles
 chezmoi init --apply kotek-7
