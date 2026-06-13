91ad79f7710b597ba5a4e37b7df70b8f8bbc9a75
kotek <scpktk@gmail.com>
2026-06-13 09:35:47 +0900

    Update README.md Update init.sh

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## Installation
 ```bash
 sudo apt-get update
-sudo apt-get install -y curl
+sudo apt-get install -y curl build-essential curl git zsh pkg-config libssl-dev
 sh -c "$(curl -fsLS https://raw.githubusercontent.com/kotek-7/dotfiles/refs/heads/main/init.sh)"
+sudo chsh -s "$(command -v zsh)" "$USER"
 ```
--- init.sh
+++ init.sh
@@ -1,17 +1,12 @@
 set -e
 
-sudo apt-get update
-sudo apt-get install -y build-essential curl git zsh pkg-config libssl-dev
-
 # setup chezmoi
 sh -c "$(curl -fsLS https://get.chezmoi.io)" -- init --apply kotek-7
 
 # setup mise
 sh -c "$(curl -fsSL https://mise.run)"
-eval "$(mise activate zsh)"
 ~/.local/bin/mise install
 
 # zsh setup
-sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
